25235b5deb6cfab58442645fa057c66b3caa292b
Rishu Gupta <rishukumargupta8409@gmail.com>
2025-12-19 00:00:05 +0530

    change updates

---
+++ app.py
@@ -0,0 +1,36 @@
+from langchain_community.document_loaders import PyPDFLoader
+from pathlib import Path
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def read_splitter(filename):
+    pdf_path=Path(__file__).parent/"docs"/f"{filename}"
+
+    loader = PyPDFLoader(pdf_path)
+    docs=loader.load()
+
+    ## split the docs into small chunks
+    chunk_splitter=RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=500
+    )
+
+    ## here we are giving the doc to chunk splitter instance.
+    chunks=chunk_splitter.split_documents(docs)
+
+    return chunks
+
+### calling here read splitter function
+#read_splitter("Practical NLP.pdf")
+
+## Vector embeding
+def vector_embedding(chunks_data):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
+    vector = embeddings.embed_query(chunks_data)
+    return vector
+
+ve=vector_embedding("hello, world!")
+print(ve[:5])
